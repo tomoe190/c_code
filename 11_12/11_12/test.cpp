7ffb107d99bc0c13eced4398b986424c8e4695f0
tomoe <593575498@qq.com>
2020-11-12 18:07:03 +0800

    指针初识

---
+++ 11_12/11_12/test.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
+/*void main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int ar[3] = { a, b, c };
+
+	//指针数组，存放指针的数组
+	int *br[3] = { &a, &b, &c };
+	int(*cr)[3];     //数组指针
+}
+
+
+/*void main()
+{
+	int a = 100;
+	int *p = &a;
+	int **s = &p;
+}
+
+/*void main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	int *p1 = &arr[0];     //首地址给它
+	int *p2 = arr;         //把数组名里的内容赋值给他
+
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d", *(p2 + i));
+		printf("%d", p2[i]);
+		printf("%d",*(arr+i));
+	}
+	printf("\n");
+}*/
