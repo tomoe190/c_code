5530117502d77f16849404bad493a4cdc968b366
tomoe <593575498@qq.com>
2020-11-25 23:46:02 +0800

    练习增加

--- 11_25/11_25/11_25.c
+++ 11_25/11_25/11_25.c
@@ -2,6 +2,60 @@
 #include<stdio.h>
 #include<Windows.h>
 
+
+
+void main()
+{
+	char *str = "abcxyz";
+	printf("str=%s\n", str);
+	printf("str=%s\n", str + 3);            //xyz
+	printf("str=%s\n", &str[3]);            //xyz
+	printf("str=%s\n", &("abcxyz"[3]));     //xyz
+	printf("str=%s\n", "abcxyz" + 3);       //xyz
+}
+
+/*int main()
+{
+	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	    
+	char **cp[] = { c + 3, c + 2, c + 1, c };
+
+	char ***cpp = cp;
+
+	printf("%s\n", **++cpp);            //POINT
+	printf("%s\n", *--*++cpp + 3);      //
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
+
+/*int main()
+{
+	char *a[] = { "work", "at", "alibaba" };
+	char **pa = a;
+	pa++;
+	printf("%s\n", *pa);
+	return 0;
+}
+
+/*int main()
+{
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int *ptr1 = (int *)(&aa + 1);
+	int *ptr2 = (int *)(*(aa + 1));
+
+	printf("%d,%d\n", *(ptr1 - 1), *(ptr2 - 1));
+	return 0;
+}
+
+/*void main()
+{
+	int a[5][5];
+	int(*p)[4];
+	p = a;
+	printf("%p,%d\n", &p[4][2] - &a[4][2], &a[4][2] - &a[4][2]);
+}
+
 //写一个函数打印arr数组的内容，不使用数组下标，使用指针
 /*int main()
 {
