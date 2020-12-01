1c841d0d817c227a0a79b78648c045de4eaae998
tomoe <593575498@qq.com>
2020-12-01 23:54:00 +0800

    练习

--- test12_1/test12_1/12_1.c
+++ test12_1/test12_1/12_1.c
@@ -1,19 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-union Un
+pragma pack(8)
+typedef struct Test
 {
-	int i;
-	char c;
-};
+	char a;    //1 + 3
+	int c;     //4
+	double b;  //8
+}Test;
+
 void main()
 {
-	union Un un;
-	// 下面输出的结果是一样的吗？
-	printf("%x\n", &(un.i));
-	printf("%x\n", &(un.c));
-
-	//下面输出的结果是什么？
-	un.i = 0x11223344;  //44 33 22 11
-	un.c = 0x55;        //55 33 22 11
-	printf("%x\n", un.i);
+	printf("size = %d\n", sizeof(Test));
+	Test t;
+	t.a = 'A';
+	t.b = 12.34;
+	t.c = 100;
 }
